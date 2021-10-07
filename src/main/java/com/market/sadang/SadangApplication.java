2285da48a278c070326fb29cae9d28c0d2663f4e
LeeJun <ljun1118@ks.ac.kr>
2021-10-07 13:33:53 +0900

    Initial commit

---
+++ src/main/java/com/market/sadang/SadangApplication.java
@@ -0,0 +1,13 @@
+package com.market.sadang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SadangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SadangApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/market/sadang/SadangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.market.sadang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SadangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
