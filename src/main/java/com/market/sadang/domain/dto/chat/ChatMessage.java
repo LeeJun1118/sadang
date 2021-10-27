cf36846c7a2f8595203a68ae82f1b03d74fc8475
LeeJun <ljun1118@ks.ac.kr>
2021-10-27 17:56:53 +0900

    채팅방 생성, 채팅방 리스트 출력 - 채팅방 입장 하는 중

---
+++ src/main/java/com/market/sadang/domain/dto/chat/ChatMessage.java
@@ -0,0 +1,19 @@
+package com.market.sadang.domain.dto.chat;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ChatMessage {
+
+    public enum MessageType{
+        ENTER, TALK, JOIN
+    }
+
+    private MessageType type;
+    private String roomId;
+    private String sender;
+    private String message;
+
+}
