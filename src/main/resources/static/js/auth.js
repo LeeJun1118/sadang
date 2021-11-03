02f6b7b532dfc8d543d14c6d292637def0f09b53
LeeJun <ljun1118@ks.ac.kr>
2021-11-03 13:05:05 +0900

    로그인/로그아웃 nav 버튼 함수 timeout 시간 조절

--- src/main/resources/static/js/auth.js
+++ src/main/resources/static/js/auth.js
@@ -1,4 +1,3 @@
-updateData();
 $(document).ready(function () {
 
     var current_fs, next_fs, previous_fs; //fieldsets
@@ -200,7 +199,6 @@ $(document).ready(function () {
     })
 
 
-
     $('#updateUsername').on('blur', function (event) {
 
         var updateUserId = $('#updateUsername').val();
@@ -247,7 +245,7 @@ $(document).ready(function () {
 
 function sample6_execDaumPostcode() {
     new daum.Postcode({
-        oncomplete: function(data) {
+        oncomplete: function (data) {
             // 팝업에서 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분.
 
             // 각 주소의 노출 규칙에 따라 주소를 조합한다.
@@ -263,18 +261,18 @@ function sample6_execDaumPostcode() {
             }
 
             // 사용자가 선택한 주소가 도로명 타입일때 참고항목을 조합한다.
-            if(data.userSelectedType === 'R'){
+            if (data.userSelectedType === 'R') {
                 // 법정동명이 있을 경우 추가한다. (법정리는 제외)
                 // 법정동의 경우 마지막 문자가 "동/로/가"로 끝난다.
-                if(data.bname !== '' && /[동|로|가]$/g.test(data.bname)){
+                if (data.bname !== '' && /[동|로|가]$/g.test(data.bname)) {
                     extraAddr += data.bname;
                 }
                 // 건물명이 있고, 공동주택일 경우 추가한다.
-                if(data.buildingName !== '' && data.apartment === 'Y'){
+                if (data.buildingName !== '' && data.apartment === 'Y') {
                     extraAddr += (extraAddr !== '' ? ', ' + data.buildingName : data.buildingName);
                 }
                 // 표시할 참고항목이 있을 경우, 괄호까지 추가한 최종 문자열을 만든다.
-                if(extraAddr !== ''){
+                if (extraAddr !== '') {
                     extraAddr = ' (' + extraAddr + ')';
                 }
                 // 조합된 참고항목을 해당 필드에 넣는다.
@@ -306,31 +304,41 @@ var timerID;
     }
 });*/
 
-function updateData(){
+var i = 1;
+if (i === 1)
+    timerID = setTimeout("updateData()", 10); // 0.01초 단위로 갱신 처리
+i++;
+if (i !== 1) {
+    console.log("my Ajax 1 ===== " + i);
+    timerID = setTimeout("updateData()", 3000); // 1초 단위로 갱신 처리
+    console.log("my Ajax 2 ===== " + i);
+}
+
+function updateData() {
 
     $.ajax({
         url: "/loginCheck",
-        type:"get",
-        data:'',
+        type: "get",
+        data: '',
         dataType: 'json',
         async: false,
         contentType: "application/json; charset=utf-8",
-        cache : false,
+        cache: false,
         success: function (data) {
             if (JSON.parse(data) == 1) {
                 $("#loginVerify").text("로그아웃");
-                $("#loginVerify").css('cursor','pointer');
-                $("#loginVerify").css('color','blue');
+                $("#loginVerify").css('cursor', 'pointer');
+                $("#loginVerify").css('color', 'blue');
                 // $("#loginVerify").attr("href","/user/out");
-                $('#loginVerify').on('click',function(e){
+                $('#loginVerify').on('click', function (e) {
                     e.preventDefault();
                     location.replace("/user/out")
                 });
 
                 $("#myInfo").text("내 정보");
-                $("#myInfo").css('cursor','pointer');
-                $("#myInfo").css('color','blue');
-                $('#myInfo').on('click',function(e){
+                $("#myInfo").css('cursor', 'pointer');
+                $("#myInfo").css('color', 'blue');
+                $('#myInfo').on('click', function (e) {
                     e.preventDefault();
                     location.replace("/myPage")
                 });
@@ -338,26 +346,23 @@ function updateData(){
 
             } else {
                 $("#loginVerify").text("로그인");
-                $("#loginVerify").css('cursor','pointer');
-                $("#loginVerify").css('color','blue');
+                $("#loginVerify").css('cursor', 'pointer');
+                $("#loginVerify").css('color', 'blue');
                 // $("#loginVerify").attr("href","/login");
-                $('#loginVerify').on('click',function(e){
+                $('#loginVerify').on('click', function (e) {
                     e.preventDefault();
                     location.replace("/login")
                 });
 
                 $("#signUp").text("회원가입");
-                $("#signUp").css('cursor','pointer');
-                $("#signUp").css('color','blue');
-                $('#signUp').on('click',function(e){
+                $("#signUp").css('cursor', 'pointer');
+                $("#signUp").css('color', 'blue');
+                $('#signUp').on('click', function (e) {
                     e.preventDefault();
                     location.replace("/signup")
                 });
-
-
             }
-
         },
     });
-    timerID = setTimeout("updateData()", 1000); // 1초 단위로 갱신 처리
+
 }
