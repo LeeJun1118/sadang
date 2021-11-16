2444d73a3b0f69b932f366ccb4022d12da536f53
LeeJun <ljun1118@ks.ac.kr>
2021-11-16 16:37:51 +0900

    찜하기, 취소하기 버튼 안나오던 것 수정

--- src/main/java/com/market/sadang/service/BuyInterestedService.java
+++ src/main/java/com/market/sadang/service/BuyInterestedService.java
@@ -51,7 +51,8 @@ public class BuyInterestedService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 존재하지 않습니다."));
         String status = null;
         try {
-            status = "" + buyInterestedRepository.findByBoard(board).getInterestedStatus();
+            Member member = memberService.findByMemberRequest();
+            status = "" + buyInterestedRepository.findByBoardAndMember(board,member).getInterestedStatus();
             return status;
         } catch (Exception e) {
             return "none";
